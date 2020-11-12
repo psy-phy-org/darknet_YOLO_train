25d0b53ffb9858e0a62379f57bca4d7601c0c008
psy-phy-owner <psyphyowner@gmail.com>
2020-11-12 19:34:35 +0900

    Add new files

--- python/divide.py
+++ python/divide.py
@@ -5,15 +5,15 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 print(current_dir)
 
-current_dir = '../data/manekineko'
-g_colab_full_path='/content/darknet/data/manekineko'
+current_dir = '../data/item'
+g_colab_full_path='/content/darknet/data/item'
 
 # Percentage of images to be used for the test set
 percentage_test = 10;
 
 # Create and/or truncate train.txt and test.txt
-file_train = open('manekineko-train.txt', 'w')  
-file_test = open('manekineko-test.txt', 'w')# Populate train.txt and test.txt
+file_train = open('item-train.txt', 'w')  
+file_test = open('item-test.txt', 'w')# Populate train.txt and test.txt
 counter = 1  
 index_test = round(100 / percentage_test)  
 for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):  
